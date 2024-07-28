eeaa07740c240d512a02a8c30f4ccf68fbfc14db
Mag codes <muneagechaste@gmail.com>
2024-07-28 15:27:31 +0200

    basic auth initial commit

--- db/schema.ts
+++ db/schema.ts
@@ -1,11 +1,4 @@
-import {
-  integer,
-  text,
-  boolean,
-  pgTable,
-  timestamp,
-  uuid,
-} from "drizzle-orm/pg-core";
+import { integer, text, boolean, pgTable, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const todo = pgTable("todos", {
   id: uuid("id").defaultRandom().primaryKey(),
@@ -14,4 +7,12 @@ export const todo = pgTable("todos", {
   completed: boolean("completed").default(false).notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const users = pgTable("users", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull(),
+  password: text("password").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
 });
