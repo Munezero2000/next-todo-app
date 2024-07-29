60c645dff10aefaf29f74116378f84dc0cf9f1ff
Mag codes <muneagechaste@gmail.com>
2024-07-29 17:27:21 +0200

    added loading indicator

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Button } from './ui/button';
-import { useFormStatus } from 'react-dom';
+import React from "react";
+import { Button } from "./ui/button";
+import { useFormStatus } from "react-dom";
+import { Loader2 } from "lucide-react";
 
 interface Props {
   buttonText: string;
@@ -9,8 +10,19 @@ interface Props {
 export default function SubmitButton({ buttonText }: Props) {
   const { pending } = useFormStatus();
   return (
-    <Button disabled={pending} aria-disabled={pending} type="submit">
-      {pending ? 'Loading...' : buttonText}
+    <Button
+      disabled={pending}
+      className={buttonText.toLowerCase() === "delete" ? "bg-red-900" : ""}
+      aria-disabled={pending}
+      type="submit"
+    >
+      {pending ? (
+        <Button disabled>
+          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+        </Button>
+      ) : (
+        buttonText
+      )}
     </Button>
   );
 }
